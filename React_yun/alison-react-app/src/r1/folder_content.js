1dc8b315bf1d87da6394e744604b7e51a36ac89a
Alison <798254972@qq.com>
2018-03-05 08:17:46 +0800

    0305

--- React_yun/alison-react-app/src/r1/folder_content.js
+++ React_yun/alison-react-app/src/r1/folder_content.js
@@ -1,17 +1,94 @@
 import React,{Component} from 'react';
+import FolderS from './folders'
+import FolderNav from './folderNav'
+
+
 
 class FolderM extends Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            arr:[],
+        }
+    }
+  
+    arrChange=(id,bol)=>{    
+        let {changeActive}=this.props
+        let arr2=[]
+        arr2.push(id)
+        changeActive(arr2,bol)
+    }
+    
+    arr=[]
+    
+    getF=()=>{
+        let {dataid,data,changeActive}=this.props;
+        let childrens=[];
+        let parents=[];
+        for(var attr in data){
+            data[attr].pid==dataid?childrens.push(data[attr]):''
+        }
+        this.arr=[]
+        this.getPa(dataid)
+        parents=this.arr
+        return {c:childrens,p:parents}
+    }
+    
+    getPa=(id)=>{
+        let {data}=this.props;
+        let val=data[id]
+        if(!data[id]){return}else{this.arr.unshift(val); this.getPa(val.pid)}
+    }
+    click=()=>{
+       let {c,p}=this.getF()
+       let checkAll=c.every(e=>e.onoff)  
+       checkAll=!checkAll
+       c.forEach((e)=>{
+           this.arrChange(e.id,checkAll)
+       })
+    }
+
     render(){
+        let {dataid,data,treeMC,changeActive,choose,changeChoose,changetext,rename}=this.props;
+        let {c,p}=this.getF()        
+        let flist=c.map((e,i)=>{
+            return <FolderS {...{
+                key:i,
+                id:e.id,
+                pid:e.pid,
+                tittle:e.title, 
+                treeMC:treeMC  ,
+                arrChange :this.arrChange ,
+                onoff:e.onoff  ,
+                changeActive:changeActive,
+                data:data,
+                choose:choose,
+                changeChoose:changeChoose,
+                changetext:changetext,
+                rename:rename
+            }} />
+        })
+        let navlist=p.map((e,i)=>{
+            return <FolderNav {...{
+                key:i,
+                id:e.id,
+                pid:e.pid,
+                tittle:e.title,
+                onoff:(i==(p.length-1))?true:false ,
+                treeMC:treeMC         
+            }} />
+        })
+        let checkAll=c[0]!=undefined && c.every(e=>e.onoff)
+        let cNameonoff=checkAll?'checkedAll checked':'checkedAll'
+ 
         return (
             <div className="folder-content">
                 <div className="breadmenu">
                     <div className="checkall">
-                        <i className="checkedAll"></i>
+                        <i className={cNameonoff} onClick={this.click} ></i>
                     </div>
                     <div className="bread-nav">
-                        {/* <!-- <a href="javascript:;">微云</a> --> */}
-                        
-                            
+                        {navlist}
                     </div>
                 </div>
                 <div className="f-empty"></div>
@@ -23,12 +100,7 @@ class FolderM extends Component {
                     <span className="sh">分享</span>
                 </div>
                 <div className="folders">
-                    <div className="file-item">
-                    <img src={require("./img/folder-b.png")} alt="" />
-                    <span className="folder-name">JS基础课程</span>
-                    <input type="text" className="editor"/>
-                    <i className="checked"></i>
-                    </div>
+                    {flist}
                 </div>
             </div>
         )
--- React_yun/alison-react-app/src/r1/head.js
+++ React_yun/alison-react-app/src/r1/head.js
@@ -3,6 +3,64 @@ import LogoM from './logo'
 import NavR from './nav_r'
 
 class HeadM extends Component {
+    remove=()=>{
+        let {changemove,data,changetext}=this.props
+        let choose=''
+        let num=0
+        for(var attr in data){          
+            if(data[attr].onoff){
+                num++ 
+                choose=attr
+            }         
+        }
+        if(num==0){
+            changetext('请选择一个文件')
+        }else{
+            changemove()
+        }   
+
+    }
+    rename=()=>{
+        let {rename,data,changetext,changeChoose}=this.props
+        let num=0
+        let choose=''
+        for(var attr in data){          
+            if(data[attr].onoff){
+                num++ 
+                choose=attr
+            }         
+        }
+        console.log(num,choose)
+        if(num>1){
+            changetext('只能选择一个文件')
+        }else if(num==0){
+            changetext('请选择一个文件')
+        }else{
+            changeChoose(choose)
+        }
+     
+    }
+    del=()=>{
+        let {changedelete,data,changetext}=this.props
+        let num=0
+        let choose=''
+        for(var attr in data){          
+            if(data[attr].onoff){
+                num++ 
+                choose=attr
+            }         
+        }
+        if(num==0){
+            changetext('请选择一个文件')
+        }else{
+            changedelete()
+        }         
+    }
+    creat=()=>{
+        let {creat,dataid}=this.props
+        creat(dataid)
+            
+    }
 
     render(){
         return (
@@ -12,10 +70,10 @@ class HeadM extends Component {
                     <ul className="nav_left">
                         <li><i></i>下载</li>
                         <li><i></i>分享</li>
-                        <li className="remove"><i></i>移动到</li>
-                        <li className="rename"><i></i>重命名</li>
-                        <li className="del"><i></i>删除</li>
-                        <li className="create"><i></i>新建文件夹</li>
+                        <li className="remove" onClick={this.props.time?()=>{}:this.remove}><i></i>移动到</li>
+                        <li className="rename" onClick={this.props.time?()=>{}:this.rename} ><i></i>重命名</li>
+                        <li className="del" onClick={this.props.time?()=>{}:this.del}><i></i>删除</li>
+                        <li className="create" onClick={this.props.time?()=>{}:this.creat}><i></i>新建文件夹</li>
                         <li><i></i></li>
                     </ul>
                     <NavR />
