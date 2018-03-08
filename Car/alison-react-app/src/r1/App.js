a3db566fcf796c99fdb6f3281542c8ca531bf0f1
Alison <798254972@qq.com>
2018-03-08 21:12:44 +0800

    0308

---
+++ Car/alison-react-app/src/r1/App.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import './App.css';
+
+
+class App extends Component {
+    constructor(){
+      super();
+
+    }
+
+       
+    render(){ 
+      
+      return (
+        <div className="box">
+           
+        </div>       
+      )
+    } 
+ }
+
+export default App
